7d7915ab14caccff5b2018cea5ed434b44c31667
Ansh sharma <116175461+Anshsharma25@users.noreply.github.com>
2024-02-16 17:54:48 +0530

    fucntionality of dice in js

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log("ansh sharma")
+console.log("ansh sharma");
+console.log("here , I am start to finish this ");
+
+const myArray = ["images/dice_2.png", "images/dice_2.png", "images/dice_6.png", "images/dice_5.png", "images/dice_4.png", "images/dice_3.png"];
+
+document.getElementById('my-button').addEventListener('click', () => {
+    const randomElement = myArray[Math.floor(Math.random() * myArray.length)];
+
+    console.log(randomElement, "---randomElement----");
+    document.getElementById('image').src = randomElement
+});
+// document.getElementsByClassName("second_box").
+
+// let color = document.getElementsByClassName("second_box");
+// var changeBorder = document.getElementsByClassName("changeBorder");
+// changeBorder.addEventListener("click", ()=> {
+//     color.style.border = "3px solid green";
+// });  
+
+document.getElementById("second_box").style.backgroundColor = "gray";
+
+
+console.log("=========================================");
+document.getElementById('gitti_down4').addEventListener('click', 
+function move_gitti() {
+    // if ("gitti_down4" = true) {
+    //     return document.getElementById('g1').append(document.getElementById("move"));
+    // }
+    // else {
+    //     return 0;
+    // } 
+    console.log("----ekfmkdmss")
+    document.getElementById('move').append(document.getElementById("gitti_down4"));
+})
+console.log(move_gitti);
+
