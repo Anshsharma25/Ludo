1ad2a500b36eb46d0a3403b9b50fe29610f2f002
Ansh sharma <116175461+Anshsharma25@users.noreply.github.com>
2024-02-28 18:50:57 +0530

    update in ui and js

--- index.js
+++ index.js
@@ -33,46 +33,52 @@ document.getElementById('my-button').addEventListener('click', () => {
     currentTurn = (currentTurn + 1) % boxArray.length;
     document.getElementById(boxArray[currentTurn]).style.backgroundColor = "gray";
 });
+
+
+
 document.getElementById('first_blueimage').addEventListener('click',
     function move_gitti() {
         var number = +objectb.b1.match(/\d+/)[0];
-        var newNumber = number + 6;
-        var newString = objectb.b1.replace(/\d+/, newNumber);
+        var newNum = number + 5;
+        var newStr = objectb.b1.replace(/\d+/, newNum);
 
-        console.log(newString);
-
+        console.log(newStr);
         console.log(document.getElementById("first_blueimage"))
-        let data = false
-        if (data) {
-            document.getElementsByClassName(objectb.b1)[0].removeChild(document.getElementById("first_blueimage"))
+
+
+
+        var elements = document.getElementsByClassName(newStr);
+        if (elements.length > 0) {
+            var firstElement = elements[0];
+            firstElement.appendChild(document.getElementById("first_blueimages"));
+            console.log(firstElement.className);
+            objectb.b1 = firstElement.className;
+        } else {
+            console.log("No element found with class name", newStr);
         }
-        document.getElementsByClassName(newString)[0].appendChild(document.getElementById("first_blueimage"));
-        const className = document.getElementsByClassName(newString)[0].className
-        console.log(className)
-        object.p1 = className
-        console.log(objectb.b1, "p11")
-        data = true
+
+
+        // when it moves another home thats why we create the arr of middle once
+        // let arr = ['Token_moving13', 'Token_moving26', 'Token_moving51'];
+        // if (arr.includes(newStr)) {
+        //     document.getElementsByClassName('Token_moving1')[0].appendChild(document.getElementById('fourthyellowimg'));
+        // }
     })
 
+
 //  this is  gotti is for purple color
-console.log("=========================================");
+
 document.getElementById('first_purpleimage').addEventListener('click',
     function move_gitti() {
         console.log("----ekfmkdmss")
         var number = +object.p1.match(/\d+/)[0];
-
-
         var newNumber = number + 5;
         var newString = +object.p1.replace(/\d+/, newNumber);
 
         console.log(newString);
         // debugger
         console.log(document.getElementById("first_purpleimage"))
-        let data = false
-        if (data == true) {
-            document.getElementsByClassName(object.p1)[0].removeChild(document.getElementById("first_purpleimage"))
-        }
-        document.getElementsByClassName(newStrixng)[0].appendChild(document.getElementById("first_purpleimage"));
+        document.getElementsByClassName(newString)[0].appendChild(document.getElementById("first_purpleimage"));
         const className = document.getElementsByClassName(newString)[0].className
         console.log(className)
         object.p1 = className
@@ -84,53 +90,65 @@ document.getElementById('first_purpleimage').addEventListener('click',
 
 //  this is  gotti is for red color-------------------------
 
-console.log("=========================================");
 document.getElementById('second_redimage').addEventListener('click',
     function move_gitti() {
         console.log("----ekfmkdmss")
         // document.getElementById('redimage').append(document.getElementById("first_redimage"));
         // document.getElementById('move').append(document.getElementById("image3"));
         var number = +objectr.r1.match(/\d+/)[0];
-
-
         var newNum = number + 5;
         var newStr = objectr.r1.replace(/\d+/, newNum);
-
         console.log(newStr);
-        // debuggers
         console.log(document.getElementById("second_redimage"))
-        let data = false
-        if (data == true) {
-            document.getElementsByClassName(objectr.r1)[0].removeChild(document.getElementById("second_redimage"))
-        }
         document.getElementsByClassName(newStr)[0].appendChild(document.getElementById("second_redimage"));
         const className = document.getElementsByClassName(newStr)[0].className
         console.log(className)
         object.p1 = className
         console.log(objectr.r1, "r11")
-        data = true
     })
 //  this is  gotti is for yellow color
-console.log("=========================================");
+let yellowOneRound = 0;
+
 document.getElementById('fourthyellowimg').addEventListener('click',
     function move_gitti() {
-        console.log("----ekfmkdmss")
-        var number = +objecty.y1.match(/\d+/)[0];
-        var newNum = number + 5;
-        var newStr = objecty.y1.replace(/\d+/, newNum);
+        // let round1 = document.getElementsByClassName("Token_moving39")
+        // let round2 = document.getElementsByClassName("Token_moving34")
+        let arr = ['yellow1', 'yellow2', 'yellow3', 'yellow4', 'yellow5']
+        let newy = arr;
+        // if (round1 || round2) {
+        //     console.log('9999999999999999999s')
+        //     document.getElementsByClassName(newy[0])[0].appendChild(document.getElementById("fourthyellowimg"));
+        // }
 
-        console.log(newStr);
-        console.log(document.getElementById("fourthyellowimg"))
+        var number = +objecty.y1.match(/\d+/)[0];
+        
+        if (number > 31 && number < 40) {
+            yellowOneRound = 2;
+        }
+        
+        if (yellowOneRound == 0) {
+            newNum = 41; yellowOneRound = 1;
+        }else {
+            newNum = number + 5;
+        }
+
+        if ((yellowOneRound == 2) && (newNum > 38)) {
+            newNum = `yellow_${newNum - 38}`
+        }
+        console.log(yellowOneRound, newNum);
+        if (newNum > 52) {
+            newNum = newNum - 52;
+        }
+        var newStr = objecty.y1.replace(/\d+/, newNum);
+        console.log(document.getElementsByClassName('Token_movingyellow_4')[0]);
+        console.log('________________', newStr);
+        console.log('=============', document.getElementsByClassName(newStr)[0]);
         document.getElementsByClassName(newStr)[0].appendChild(document.getElementById("fourthyellowimg"));
         const className = document.getElementsByClassName(newStr)[0].className
         console.log(className)
         objecty.y1 = className
         console.log(objecty.y1, "y11")
-        data = true
 
-        let arr = ['Token_moving53', 'Token_moving54', 'Token_moving55', 'Token_moving56', 'Token_moving57', 'blue']
-        console.log(arr.includes(newStr))
-        if (arr == true) {
-            return document.getElementById('fourthyellowimg').appendChild(document.getElementsByClassName('Token_moving1'))
-        }
+        // when it moves another home thats why we create the arr of middle once
+
     })
